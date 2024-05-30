ce1a67e0f6a166e879121f480208ca4fc8b0cc5a
Astermer_ter <63912145+Astermerter@users.noreply.github.com>
2024-05-30 16:44:19 +0500

    Восстановление кодировки русского текста

--- SquareAP.h
+++ SquareAP.h
@@ -1,10 +1,10 @@
 #ifndef SquareAP
 #define SquareAP
 
-void AddInfInAllArr(float* x, float* y, int n, float* A_arr, float* B_arr, float* C_arr); // Функция, которая записывает все коэффициенты в массивы
-int find_Av_a(float x1, float y1, float x2, float y2, float x3, float y3); // функция, которая находит коэффициент a
-int find_Av_b(float x1, float y1, float x2, float y2, float x3, float y3); // функция, которая находит коэффициент b
-int find_Av_c(float x1, float y1, float x2, float y2, float x3, float y3); // функция, которая находит коэффициент c
-float return_Av_N(float* N, float n); // Функция, которая находит среднее по массиву
+void AddInfInAllArr(float* x, float* y, int n, float* A_arr, float* B_arr, float* C_arr); // Р¤СѓРЅРєС†РёСЏ, РєРѕС‚РѕСЂР°СЏ Р·Р°РїРёСЃС‹РІР°РµС‚ РІСЃРµ РєРѕСЌС„С„РёС†РёРµРЅС‚С‹ РІ РјР°СЃСЃРёРІС‹
+int find_Av_a(float x1, float y1, float x2, float y2, float x3, float y3); // С„СѓРЅРєС†РёСЏ, РєРѕС‚РѕСЂР°СЏ РЅР°С…РѕРґРёС‚ РєРѕСЌС„С„РёС†РёРµРЅС‚ a
+int find_Av_b(float x1, float y1, float x2, float y2, float x3, float y3); // С„СѓРЅРєС†РёСЏ, РєРѕС‚РѕСЂР°СЏ РЅР°С…РѕРґРёС‚ РєРѕСЌС„С„РёС†РёРµРЅС‚ b
+int find_Av_c(float x1, float y1, float x2, float y2, float x3, float y3); // С„СѓРЅРєС†РёСЏ, РєРѕС‚РѕСЂР°СЏ РЅР°С…РѕРґРёС‚ РєРѕСЌС„С„РёС†РёРµРЅС‚ c
+float return_Av_N(float* N, float n); // Р¤СѓРЅРєС†РёСЏ, РєРѕС‚РѕСЂР°СЏ РЅР°С…РѕРґРёС‚ СЃСЂРµРґРЅРµРµ РїРѕ РјР°СЃСЃРёРІСѓ
 
 #endif // !SquareAP
